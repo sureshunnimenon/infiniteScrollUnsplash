5c2e7526f7a2f6dd3f44d8a028bd6a72bec54de4
sureshunnimenon <suresh.unni.menon@gmail.com>
2019-02-22 10:03:44 +0300

    initial

---
+++ config/server.js
@@ -0,0 +1,8 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '222b9d2d5e601b9079df81478061e839a2829aa21bb444d3e59670c9b32b4158',
+        // this is the ACCESS KEY
+
+    SECRET: process.env.SECRET || '02c4e74ef3c4ea90b5cde84135f3178fba492327bdf8f9fde872306de055afc8',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000' // node server on port 5000, react/frontend on 3000
+
+}
